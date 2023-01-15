478b77aafbec36e670d5c72b0e7014b8404271e9
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-01-15 07:41:09 -0600

    Position grid in the center of page and set standard size of each pixel

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.container');
 
-for (let i =0; i < 16; i++) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixel.textContent = " " + i + " ";
-    container.appendChild(pixel);
+for (let i = 0; i < 16; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    container.appendChild(column);
+    for (let j = 0; j < 16; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.textContent = " ";
+        column.appendChild(pixel);
+    }   
 }
