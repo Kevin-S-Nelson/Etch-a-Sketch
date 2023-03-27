17261baa9412f200132d1ef5d99d633daf1d3c23
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-03-27 05:58:44 -0500

    Add functionality to the button, allowing for it to change the size of the grid

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const container = document.querySelector('.container');
 
 let gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    container.appendChild(column);
-    for (let j = 0; j < gridSize; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.textContent = " ";
-        column.appendChild(pixel);
-    }   
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
+        for (let j = 0; j < gridSize; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.textContent = " ";
+            column.appendChild(pixel);
+        }   
+    }
+}
+
+createGrid();
+
+function removeGrid() {
+    container.innerHTML = "";
+}
+
+function changeGridSize() {
+    removeGrid();
+    gridSize = 32;
+    console.log("girdSize = " + gridSize);
+    createGrid();
 }
